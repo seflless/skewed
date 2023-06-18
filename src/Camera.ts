e0a521d049e959e565f70d22415a804d5b94b296
Francois <francoislaberge@gmail.com>
2023-06-18 14:07:42 -0400

    Added Cabinet projection, though haven't tested it yet

--- src/Camera.ts
+++ src/Camera.ts
@@ -4,3 +4,16 @@ export function point3DToIsometric(x: number, y: number, z: number) {
     y: (x + z) * Math.sin(Math.PI / 6) - y + window.innerHeight / 2,
   };
 }
+
+export function point3DToCabinet(x: number, y: number, z: number) {
+  // Oblique angle usually is 45 degrees for cabinet projection.
+  let alpha = Math.PI / 4;
+
+  // scale factor typically half in depth (z) direction.
+  let scale = 0.5;
+
+  return {
+    x: x + scale * z * Math.cos(alpha) + window.innerWidth / 2,
+    y: y + scale * z * Math.sin(alpha) + window.innerHeight / 2,
+  };
+}
